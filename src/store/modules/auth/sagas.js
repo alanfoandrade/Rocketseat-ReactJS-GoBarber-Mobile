09e3bf7d4ec1feafead300b7b3e89a34780b2e99
alanfoandrade <alanfrank@gec.inatel.br>
2019-12-09 22:24:15 -0300

    BUGOU TUDO - Adicionado o AsyncStorage

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,77 @@
+import { Alert } from 'react-native';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+
+import { signInSuccess, signFailure } from './actions';
+
+import api from '~/services/api';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, '/sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (user.provider) {
+      Alert.alert(
+        'Falha no login',
+        'Prestador de serviços não pode ser cliente'
+      );
+      return;
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    // history.push('/dashboard');
+  } catch (err) {
+    Alert.alert('Falha na autenticação', 'Verifique os dados, tente novamente');
+
+    yield put(signFailure());
+  }
+}
+
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    // history.push('/');
+  } catch (err) {
+    Alert.alert('Falha no cadastro', 'Verifique seus dados, tente novamente');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  // history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
---
+++ src/store/persistReducers.js
@@ -0,0 +1,17 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import { persistReducer } from 'redux-persist';
+
+console.tron.log(AsyncStorage);
+
+export default reducers => {
+  const persistedReducer = persistReducer(
+    {
+      key: 'gobarber',
+      storage: AsyncStorage,
+      whitelist: ['auth', 'user'],
+    },
+    reducers
+  );
+
+  return persistedReducer;
+};
