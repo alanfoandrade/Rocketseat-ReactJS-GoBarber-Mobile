dc2fda605d618ed9d972cfb2958e32e54fe24aea
alanfoandrade <alanfrank@gec.inatel.br>
2019-12-09 21:16:40 -0300

    Criacao da base do projeto

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import './config/ReactotronConfig';
+
+export default function App() {
+  console.tron.log('oi');
+  return <Text>TA ROLANDO!!</Text>;
+}
